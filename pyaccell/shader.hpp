850f458cbdeb3b7e3700012581ce7eb5124872ec
Syed Ahmed Hussain <ahmedhus22@gmail.com>
2024-05-05 02:05:56 +0530

    fixed shader header and added namespace

--- pyaccell/shader.hpp
+++ pyaccell/shader.hpp
@@ -3,18 +3,21 @@
 
 #include <string>
 
-class Shader {
-    public:
-        // constructor generates the shader on the fly
-        Shader(const char* vertexPath, const char* fragmentPath);
-        // activate the shader
-        void use();
-        // utility uniform functions
-        void setBool(const std::string &name, bool value) const;
-        void setInt(const std::string &name, int value) const;
-        void setFloat(const std::string &name, float value) const;
-    private:
-        // utility function for checking shader compilation/linking errors.
-        void checkCompileErrors(unsigned int shader, std::string type);
-};
+namespace pyaccell {
+    class Shader {
+        public:
+            unsigned int ID;
+            // constructor generates the shader on the fly
+            Shader(const char* vertexPath, const char* fragmentPath);
+            // activate the shader
+            void use();
+            // utility uniform functions
+            void setBool(const std::string &name, bool value) const;
+            void setInt(const std::string &name, int value) const;
+            void setFloat(const std::string &name, float value) const;
+        private:
+            // utility function for checking shader compilation/linking errors.
+            void checkCompileErrors(unsigned int shader, std::string type);
+    };
+}
 #endif
