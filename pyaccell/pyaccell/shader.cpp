acc6c06024bcb543d3d90d0fd35b50a7e931f1b3
Syed Ahmed Hussain <ahmedhus22@gmail.com>
2024-05-18 17:46:10 +0530

    add setVec and setMat to Shader class

--- pyaccell/pyaccell/shader.cpp
+++ pyaccell/pyaccell/shader.cpp
@@ -1,5 +1,6 @@
 #include <pyaccell/shader.hpp>
 #include <glad/glad.h>
+#include <glm/glm.hpp>
 
 #include <string>
 #include <fstream>
@@ -82,6 +83,48 @@ void pyaccell::Shader::setInt(const std::string &name, int value) const
 void pyaccell::Shader::setFloat(const std::string &name, float value) const
 { 
     glUniform1f(glGetUniformLocation(ID, name.c_str()), value); 
+}
+
+void pyaccell::Shader::setVec2(const std::string &name, const glm::vec2 &value) const
+{ 
+    glUniform2fv(glGetUniformLocation(ID, name.c_str()), 1, &value[0]); 
+}
+void pyaccell::Shader::setVec2(const std::string &name, float x, float y) const
+{ 
+    glUniform2f(glGetUniformLocation(ID, name.c_str()), x, y); 
+}
+// ------------------------------------------------------------------------
+void pyaccell::Shader::setVec3(const std::string &name, const glm::vec3 &value) const
+{ 
+    glUniform3fv(glGetUniformLocation(ID, name.c_str()), 1, &value[0]); 
+}
+void pyaccell::Shader::setVec3(const std::string &name, float x, float y, float z) const
+{ 
+    glUniform3f(glGetUniformLocation(ID, name.c_str()), x, y, z); 
+}
+// ------------------------------------------------------------------------
+void pyaccell::Shader::setVec4(const std::string &name, const glm::vec4 &value) const
+{ 
+    glUniform4fv(glGetUniformLocation(ID, name.c_str()), 1, &value[0]); 
+}
+void pyaccell::Shader::setVec4(const std::string &name, float x, float y, float z, float w) const
+{ 
+    glUniform4f(glGetUniformLocation(ID, name.c_str()), x, y, z, w); 
+}
+// ------------------------------------------------------------------------
+void pyaccell::Shader::setMat2(const std::string &name, const glm::mat2 &mat) const
+{
+    glUniformMatrix2fv(glGetUniformLocation(ID, name.c_str()), 1, GL_FALSE, &mat[0][0]);
+}
+// ------------------------------------------------------------------------
+void pyaccell::Shader::setMat3(const std::string &name, const glm::mat3 &mat) const
+{
+    glUniformMatrix3fv(glGetUniformLocation(ID, name.c_str()), 1, GL_FALSE, &mat[0][0]);
+}
+// ------------------------------------------------------------------------
+void pyaccell::Shader::setMat4(const std::string &name, const glm::mat4 &mat) const
+{
+    glUniformMatrix4fv(glGetUniformLocation(ID, name.c_str()), 1, GL_FALSE, &mat[0][0]);
 }
 
 
