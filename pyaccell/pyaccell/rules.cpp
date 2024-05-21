815764b2562f3bb24b98b5065b30e24b34091eea
Syed Ahmed Hussain <ahmedhus22@gmail.com>
2024-05-21 19:50:04 +0530

    fix TexParameters (caused texture to be unloadable)

--- pyaccell/pyaccell/rules.cpp
+++ pyaccell/pyaccell/rules.cpp
@@ -30,6 +30,8 @@ unsigned int pyaccell::generate_binomials() {
     glGenTextures(1, &binomial_tex);
     glBindTexture(GL_TEXTURE_2D, binomial_tex);
     glTexImage2D(GL_TEXTURE_2D, 0, GL_R32UI, 32, 32, 0, GL_RED_INTEGER, GL_UNSIGNED_BYTE, data);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
     delete[] data;
 
     return binomial_tex;
@@ -42,10 +44,12 @@ unsigned int pyaccell::generate_rule(unsigned int *rule, size_t states, size_t i
     glGenTextures(1, &textureRule);
     glBindTexture(GL_TEXTURE_2D, textureRule);
     glTexImage2D(GL_TEXTURE_2D, 0, GL_R32UI, states, indices, 0, GL_RED_INTEGER, GL_UNSIGNED_BYTE, rule);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
     return textureRule;
 }
 
-// returns texture which stores input states (initially random)
+// returns texture which stores input states (initialized to random values) stores input in red channel
 unsigned int pyaccell::random_input_state(const unsigned int width, const unsigned int height, const unsigned int states) {
     unsigned int textureInputStates;
     glGenTextures(1, &textureInputStates);
@@ -54,12 +58,8 @@ unsigned int pyaccell::random_input_state(const unsigned int width, const unsign
     srand(time(0));
     for (int i=0; i<width*height; i++) {
         data[i] = (unsigned int)(rand() % states);
-        if (data[i] == 1) {
-            data[i] = 255;
-        }
-        else data[i] = 0;
     }
-    glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, width, height, 0, GL_RGB, GL_UNSIGNED_BYTE, data);
+    glTexImage2D(GL_TEXTURE_2D, 0, GL_R32UI, width, height, 0, GL_RED_INTEGER, GL_UNSIGNED_BYTE, data);
     glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
     glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
     delete[] data;
