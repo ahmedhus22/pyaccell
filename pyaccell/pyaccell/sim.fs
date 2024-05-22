e54d0a3d36daa12953d6341a1f3098937fd35dcf
Syed Ahmed Hussain <ahmedhus22@gmail.com>
2024-05-22 17:32:05 +0530

    implement calculation of next state for 2 state CA

--- pyaccell/pyaccell/sim.fs
+++ pyaccell/pyaccell/sim.fs
@@ -1,5 +1,5 @@
 #version 330 core
-layout (location = 0) out uint FragColor;
+layout (location = 0) out uint nextstate;
 
 in vec2 TexCoords;
 
@@ -14,6 +14,17 @@ uniform int subIndices;
 void main() 
 {
     uint curstate = texture(inputStates, TexCoords).r;
+
+    int count = -int(curstate);
+    for (int x = -1; x <= 1; x += 1) {
+        for (int y = -1; y <= 1; y += 1) {
+            int v = int(texture(inputStates, TexCoords + vec2(x / inputWidth, y / inputHeight)).r);
+            if (v == 1) {
+                count += 1;
+            }
+        }
+    }
+    uint newstate = texelFetch(rule, ivec2(count, curstate), 0).r;
     
-    FragColor = curstate;
+    nextstate = newstate;
 }
