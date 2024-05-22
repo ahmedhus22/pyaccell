ef496ff04089e2115de059f2ddbedc387a6c76db
Syed Ahmed Hussain <ahmedhus22@gmail.com>
2024-05-22 16:15:58 +0530

    display input state to screen

--- pyaccell/pyaccell/framebuffers_screen.fs
+++ pyaccell/pyaccell/framebuffers_screen.fs
@@ -3,10 +3,12 @@ out vec4 FragColor;
 
 in vec2 TexCoords;
 
-uniform sampler2D screenTexture;
+uniform usampler2D states;
 
 void main()
 {
-    vec3 col = texture(screenTexture, TexCoords).rgb;
+    uint state = texture(states, TexCoords).r;
+    vec3 col = vec3(float(state));
+    
     FragColor = vec4(col, 1.0);
 } 
--- pyaccell/pyaccell/sim.fs
+++ pyaccell/pyaccell/sim.fs
@@ -1,23 +1,19 @@
 #version 330 core
-out vec4 FragColor;
+layout (location = 0) out uint FragColor;
 
 in vec2 TexCoords;
 
-uniform sampler2D uBinomial;
-uniform sampler2D inputStates;
-uniform sampler2D rule;
+uniform usampler2D uBinomial;
+uniform usampler2D inputStates;
+uniform usampler2D rule;
 uniform int numStates;
 uniform int inputWidth; // width used to create input texture
+uniform int inputHeight;
 uniform int subIndices;
-
-int binomial(int n, int k) {
-    return int(texelFetch(uBinomial, ivec2(n, k), 0).r);
-}
 
 void main() 
 {
-    int curstate = int(texture(inputStates, TexCoords).r);
+    uint curstate = texture(inputStates, TexCoords).r;
     
-    vec3 color = vec3(curstate);
-    FragColor = vec4(color, 1.0);
+    FragColor = curstate;
 }
