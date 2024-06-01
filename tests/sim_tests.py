61a8591f0581f8e67c1ff46293ce8aaa6386a5f4
Syed Ahmed Hussain <ahmedhus22@gmail.com>
2024-06-01 22:12:36 +0530

    add python test for get_index()

--- tests/sim_tests.py
+++ tests/sim_tests.py
@@ -8,4 +8,9 @@ states = 2
 output = []
 ca = pyaccell.Automata(rule, states)
 ca.run(10)
-print(pyaccell.vec_to_list(ca.output))
+print(pyaccell.vec_to_list(ca.output)[:30])
+
+n = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+states = 3
+n[3] = 8
+print(pyaccell.get_index(n, 3))
